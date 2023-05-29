f49729cec32ae1d8f87cb7daf96c5006535d1de9
venturols <52836378+venturols@users.noreply.github.com>
2023-05-29 16:23:37 +0200

    Create style.css

---
+++ style.css
@@ -0,0 +1,54 @@
+body {
+  background-image: url("images/notebook-background.jpg");
+  background-size: cover;
+  font-family: "Comic Sans MS", cursive, sans-serif;
+  color: #333;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 40px;
+  background-color: rgba(255, 255, 255, 0.8);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+
+h1 {
+  text-align: center;
+}
+
+label {
+  display: block;
+  margin-bottom: 10px;
+}
+
+input {
+  width: 100%;
+  padding: 10px;
+  margin: 0 auto 20px;
+  font-size: 16px;
+}
+
+button {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  font-size: 16px;
+  background-color: #ff0000;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+
+#result {
+  font-size: 24px;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+#events {
+  font-size: 16px;
+  line-height: 1.5;
+}
